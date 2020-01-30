d151be2ff9cf8c8a3cf5721b182c40c10e85a8a5
Selvin Dsouza <selvin.dsouza@manipalgroup.info>
2020-01-30 10:53:55 +0530

    Recognize text from image functionality added

--- app/src/main/java/com/vijnalabs/library/MainActivity.java
+++ app/src/main/java/com/vijnalabs/library/MainActivity.java
@@ -2,13 +2,65 @@ package com.vijnalabs.library;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.vijnalabs.util.Player;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivImg;
+    TextView tvOcrValue;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivImg = findViewById(R.id.ivImg);
+        tvOcrValue = findViewById(R.id.tvOcrValue);
+        submit = findViewById(R.id.submit);
+
+        Player player = new Player();
+        player.shortToast(this, "selvin");
+        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.exit);
+        //player.getTextFromImage();
     }
+
+//    public void getTextFromImage(View v) {
+//
+//        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.exit);
+//
+//        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+//
+//        if (!textRecognizer.isOperational()) {
+//
+//            Toast.makeText(getApplicationContext(), "Could not get the Text", Toast.LENGTH_LONG).show();
+//
+//        } else {
+//
+//            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+//            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+//            StringBuilder sb = new StringBuilder();
+//
+//            for (int i = 0; i < items.size(); i++) {
+//                TextBlock myItem = items.valueAt(i);
+//                sb.append(myItem.getValue());
+//                sb.append("\n");
+//            }
+//
+//            tvOcrValue.setText(sb.toString());
+//        }
+//    }
 }
--- util/src/main/java/com/vijnalabs/util/Player.java
+++ util/src/main/java/com/vijnalabs/util/Player.java
@@ -1,13 +1,51 @@
 package com.vijnalabs.util;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.SparseArray;
+import android.view.View;
 import android.widget.Toast;
 
-public class Player {
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Player extends AppCompatActivity {
 
     String name;
 
     public static void shortToast(Context context, String name) {
         Toast.makeText(context, name, Toast.LENGTH_LONG).show();
     }
+
+    public String getTextFromImage(View v, Bitmap bitmap) {
+
+        StringBuilder sb = null;
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (!textRecognizer.isOperational()) {
+
+            Toast.makeText(getApplicationContext(), "Could not get the Text", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+            SparseArray<TextBlock> items = textRecognizer.detect(frame);
+             sb = new StringBuilder();
+
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock myItem = items.valueAt(i);
+                sb.append(myItem.getValue());
+                sb.append("\n");
+            }
+
+        }
+
+        return sb.toString();
+
+    }
+
 }
